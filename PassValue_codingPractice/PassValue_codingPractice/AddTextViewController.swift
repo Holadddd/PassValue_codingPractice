8fff6cbdc5d5172f3e2dac70afa481e0dee6e68e
TH-W <wu19931221@gmail.com>
2019-08-20 15:16:57 +0800

    auto_layout done.

--- PassValue_codingPractice/PassValue_codingPractice/AddTextViewController.swift
+++ PassValue_codingPractice/PassValue_codingPractice/AddTextViewController.swift
@@ -9,22 +9,59 @@
 import UIKit
 
 class AddTextViewController: UIViewController {
+    
+    var textField: UITextField! {
+        didSet {
+            
+        }
+    }
+    
+    var checkButton: UIButton! {
+        didSet {
+
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFieldSetUp()
+        buttonSetUp()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension AddTextViewController {
+    
+    func textFieldSetUp() {
+        textField = UITextField()
+        self.view.addSubview(textField)
+        //Auto-Layout
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        //top to safe area: 100
+        textField.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        //Width 為 SuperView 的 2/3
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3 ).isActive = true
+        //水平置中
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //Height 為 40
+        textField.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        textField.borderStyle = .line
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func buttonSetUp() {
+        checkButton = UIButton()
+        self.view.addSubview(checkButton)
+        //Auto-Layout
+        checkButton.translatesAutoresizingMaskIntoConstraints = false
+        //Width 為 SuperView 的 2/3
+        checkButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3).isActive = true
+        //水平置中
+        checkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        //Height 為 40
+        checkButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        //top 在 super view 的 centerYAnchor 的下方 20 pt
+        checkButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 20).isActive = true
+        checkButton.setTitle("CHECK", for: .normal)
+        checkButton.backgroundColor = .black
     }
-    */
-
 }
