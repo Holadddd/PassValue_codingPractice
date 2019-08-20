31ea82424c1d2ecbfdb67df9f9e157d1844c917b
TH-W <wu19931221@gmail.com>
2019-08-20 14:21:19 +0800

    delete row with delegate.

---
+++ PassValue_codingPractice/PassValue_codingPractice/PassValueTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PassValueTableViewCell.swift
+//  PassValue_codingPractice
+//
+//  Created by wu1221 on 2019/8/20.
+//  Copyright © 2019 wu1221. All rights reserved.
+//
+
+import UIKit
+
+class PassValueTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var showTextLabel: UILabel!
+    
+    @IBAction func deleteButton(_ sender: UIButton) {
+        delegate?.deleteCell(self)
+    }
+    
+    
+    
+    weak var delegate: ViewController?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- PassValue_codingPractice/PassValue_codingPractice/ViewController.swift
+++ PassValue_codingPractice/PassValue_codingPractice/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var textArr = ["2", "3" , "4", "5"]
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return textArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let info = textArr[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "PassValueTableViewCell") as? PassValueTableViewCell else { fatalError() }
+        cell.showTextLabel.text = info
+        
+        cell.delegate = self
+        return cell
+    }
+    
+    
+}
+//delegate
+extension ViewController {
+    
+    func deleteCell(_ cell:PassValueTableViewCell) {
+
+        guard let indexPath = tableView.indexPath(for: cell) else { fatalError() }
+        print(indexPath)
+        textArr.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        
+    }
+    
+}
